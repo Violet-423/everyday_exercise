ada70de4e77742891e5edcefcc79395c6c24bb4f
Flying42 <1211074623@qq.com>
2024-08-01 14:21:00 +0800

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,16 @@
+#钱包余额（变量名：money），初始余额50
+"""
+冰淇淋10元
+可乐5元
+"""
+money=50
+print("当前钱包余额：",money,"元")
+money1=10
+print("购买了冰淇淋，花费：",money1,"元")
+money2=5
+print("购买了可乐，花费：",money2,"元")
+money=money-money1-money2
+print("最终，钱包剩余：",money,"元")
+
+
+
